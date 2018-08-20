e218aaf4ef024c9c725e1a4abb08e9b6d8eba2a2
Kafil <kafil.nasdami@gmail.com>
2018-08-20 12:36:56 +0100

    Using prettier to format code

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const app = require('express')();
 
 app.get('/', (req, res) => {
-    res.json({ msg: 'working !' })
-})
+  res.json({ msg: 'working !' });
+});
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Listening on port ${port}`))
+app.listen(port, () => console.log(`Listening on port ${port}`));
