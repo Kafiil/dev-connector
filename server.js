018b574d78a4ee33463d19d9bf6bb688ad1a8e61
Kafil <kafil.nasdami@gmail.com>
2018-08-20 12:27:03 +0100

    Performing initial settunp

---
+++ server.js
@@ -0,0 +1,9 @@
+const app = require('express')();
+
+app.get('/', (req, res) => {
+    res.json({ msg: 'working !' })
+})
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Listening on port ${port}`))
