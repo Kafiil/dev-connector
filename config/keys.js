d0ecfef7569286a48a225591de11c7c78e800fb6
Kafil <kafil.nasdami@gmail.com>
2018-08-27 23:27:05 +0100

    Using a local DB instead of Mlab

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
-  mongoUrl: 'mongodb://root:root12@ds225492.mlab.com:25492/mern-connector',
+  // mongoUrl: 'mongodb://root:root12@ds225492.mlab.com:25492/mern-connector',
+  mongoUrl: 'mongodb://localhost:27017/mine',
   secretOrKey: 'secret'
 };
