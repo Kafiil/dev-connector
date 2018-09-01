34c95cadc4fae5f4303e3653a4492384d61497ab
Unknown <kafil.nasdami@gmail.com>
2018-09-01 16:03:33 +0100

    Implementing react routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
+import { BrowserRouter, Route } from 'react-router-dom';
+
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
 import Landing from './components/layout/Landing';
@@ -7,11 +9,13 @@ import Landing from './components/layout/Landing';
 class App extends Component {
   render() {
     return (
-      <div>
-        <Navbar />
-        <Landing />
-        <Footer />
-      </div>
+      <BrowserRouter>
+        <div>
+          <Navbar />
+          <Route exact path="/" component={Landing} />
+          <Footer />
+        </div>
+      </BrowserRouter>
     );
   }
 }
