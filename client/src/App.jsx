164af6e62824540cd58289b7efe20451401cdd20
Kafil Nasdami <kafil.nasdami@gmail.com>
2018-09-02 13:03:52 +0100

    Set up Redux

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,30 +1,36 @@
 import React, { Component } from 'react';
-import './App.css';
+//React-router
 import { BrowserRouter, Route } from 'react-router-dom';
+//Redux
+import { Provider } from 'react-redux';
+import store from './store';
 
+// components
 import MainNavbar from './components/layout/MainNavbar';
 import Footer from './components/layout/Footer';
 import Landing from './components/layout/Landing';
-
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
 
-class App extends Component {
+// Css
+import './App.css';
+
+export default class App extends Component {
   render() {
     return (
-      <BrowserRouter>
-        <div>
-          <MainNavbar />
-          <Route exact path="/" component={Landing} />
-          <div className="container">
-            <Route exact path="/register" component={Register} />
-            <Route exact path="/login" component={Login} />
+      <Provider store={store}>
+        <BrowserRouter>
+          <div>
+            <MainNavbar />
+            <Route exact path="/" component={Landing} />
+            <div className="container">
+              <Route exact path="/register" component={Register} />
+              <Route exact path="/login" component={Login} />
+            </div>
+            <Footer />
           </div>
-          <Footer />
-        </div>
-      </BrowserRouter>
+        </BrowserRouter>
+      </Provider>
     );
   }
 }
-
-export default App;
